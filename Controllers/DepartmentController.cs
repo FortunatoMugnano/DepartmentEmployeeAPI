6977fdc84dcfd597f67a84d363eb6ca8c762127b
unknown <fortunato.mugnano88@gmail.com>
2019-12-11 16:36:39 -0600

    first commit

---
+++ Controllers/DepartmentController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using DepartmentExample.Model;
+using Microsoft.AspNetCore.Http;
+using System.Data.SqlClient;
+
+namespace DepartmentExample.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public DepartmentController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Id, DeptName FROM Department";
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    List<Department> departments = new List<Department>();
+
+                    while (reader.Read())
+                    {
+                        Department department = new Department
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            DeptName = reader.GetString(reader.GetOrdinal("DeptName"))
+                        };
+
+                        departments.Add(department);
+                    }
+                    reader.Close();
+
+                    return Ok(departments);
+                }
+            }
+        }
+
+        [HttpGet("{id}", Name = "GetDepartment")]
+        public async Task<IActionResult> Get([FromRoute] int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT
+                            Id, DeptName
+                        FROM Department
+                        WHERE Id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    Department department = null;
+
+                    if (reader.Read())
+                    {
+                        department = new Department
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            DeptName = reader.GetString(reader.GetOrdinal("DeptName"))
+                         
+                        };
+                    }
+                    reader.Close();
+
+                    if (department == null)
+                    {
+                        return NotFound($"No Department found with the id of {id}");
+                    }
+                    return Ok(department);
+                }
+            }
+        }
+    }
+}
